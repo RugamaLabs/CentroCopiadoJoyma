98d87944184122c544a0fc6435c73585e117795c
Jonathan Rugama <jrugama96@gmail.com>
2026-03-12 13:25:30 -0600

    comment: explanation added about optional library

--- src/main/java/jrugama/centrofotocopiadojoyma/repository/AppUserRepository.java
+++ src/main/java/jrugama/centrofotocopiadojoyma/repository/AppUserRepository.java
@@ -2,6 +2,10 @@ package jrugama.centrofotocopiadojoyma.repository;
 
 import jrugama.centrofotocopiadojoyma.model.AppUser;
 import java.util.List;
+// Optional se utiliza para encapsular de forma segura el resultado de una consulta que podría 
+// no encontrar ningún registro (es decir, evitar que se retorne null y cause NullPointerException).
+// En esta interfaz se implementa en los métodos de búsqueda (findById, findByEmail, findByName) 
+// porque es posible que el usuario que se está buscando no exista en la base de datos.
 import java.util.Optional;
 
 public interface AppUserRepository {
